9b658e8e40a607db7c92eb204201c6d63ecd60df
Cagdas Can <ccan@alpha-sense.com>
2022-10-30 21:30:31 +0200

    url shortening first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,72 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
 
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+  const [alias, setAlias] = useState("");
+
+  const shorten = trpc.example.shorten.useMutation({
+    onSuccess: (data) => {
+      setAlias(data.shortened);
+    },
+  });
+
+  const handleClick = () => {
+    shorten.mutate({ url: "https://trpc.io" });
+  };
 
   return (
-    <>
-      <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
-        {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+    <div className="flex h-full min-h-screen w-full flex-col items-center justify-center bg-slate-900 pt-6 text-2xl text-slate-300">
+      <div>Shorten your links</div>
+      <div>Create and monitor your links. It’s secure, fast and free.</div>
+      <div className="mt-4 flex w-1/4 flex-col">
+        <input
+          type="text"
+          id="alias"
+          className="mb-2 bg-white text-slate-900"
+        />
+        <input
+          type="text"
+          id="url"
+          className="mb-2 bg-white text-slate-900"
+          value={alias}
+        />
+        <button
+          type="button"
+          className="bg-indigo-600 text-white"
+          onClick={handleClick}
+        >
+          Shorten
+        </button>
       </div>
-      <AuthShowcase />
-    </>
+    </div>
   );
 };
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
+// const AuthShowcase: React.FC = () => {
+//   const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
 
-  const { data: sessionData } = useSession();
+//   const { data: sessionData } = useSession();
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {sessionData && (
-        <p className="text-2xl text-blue-500">
-          Logged in as {sessionData?.user?.name}
-        </p>
-      )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
-      <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-2">
+//       {sessionData && (
+//         <p className="text-2xl text-blue-500">
+//           Logged in as {sessionData?.user?.name}
+//         </p>
+//       )}
+//       {secretMessage && (
+//         <p className="text-2xl text-blue-500">{secretMessage}</p>
+//       )}
+//       <button
+//         className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
+//         onClick={sessionData ? () => signOut() : () => signIn()}
+//       >
+//         {sessionData ? "Sign out" : "Sign in"}
+//       </button>
+//     </div>
+//   );
+// };
--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,16 +1,23 @@
 import { z } from "zod";
+import { prisma } from "../../../server/db/client";
 
 import { router, publicProcedure } from "../trpc";
+import ShortUniqueId from "short-unique-id";
 
 export const exampleRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
+  shorten: publicProcedure
+    .input(z.object({ url: z.string() }))
+    .mutation(async ({ input: { url } }) => {
+      const uid = new ShortUniqueId({ length: 5 });
+      const id = uid();
+      const { alias } = await prisma.url.create({
+        data: {
+          url,
+          alias: id,
+        },
+      });
       return {
-        greeting: `Hello ${input?.text ?? "world"}`,
+        shortened: alias,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
 });
